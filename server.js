b667b0ef7c21c67cd4b163b4ca9546ed6953a61e
eui <euijoon21@naver.com>
2024-01-11 00:34:58 +0900

    update_2

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+
+app.set('view engine', 'ejs'); 
+
+
 app.listen('3000', () => {
     console.log('http://localhost:3000');
 })
 
 app.get("/", (req, res) => {
-    res.send("hello")
+    res.render('index.ejs');
 })
