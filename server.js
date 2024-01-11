727f82f71d3ab4eb49a4caa7570905366826b521
eui <euijoon21@naver.com>
2024-01-11 15:24:12 +0900

    240111_update_1

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ObjectId } = require('mongodb')
 
+let db
+const url = 'mongodb+srv://eui:mQAudYjjXn2wMqP9@forum.9n9hsli.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+}).catch((err)=>{
+  console.log(err)
+})
 
 app.set('view engine', 'ejs'); 
 
@@ -11,4 +20,10 @@ app.listen('3000', () => {
 
 app.get("/", (req, res) => {
     res.render('index.ejs');
+})
+
+app.get("/list", async (req, res) => {
+    let result = await db.collection('post').find().toArray();
+    console.log(result);
+    res.render('list.ejs', { result });
 })
